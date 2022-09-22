f4c9373a2f767c4dce546353d0958b4b8e1201a0
kang.xin <kang.xin@yoomigroup.com>
2022-09-22 17:58:15 +0800

    add:  添加自选字和结果数

---
+++ src/scripts/config.js
@@ -0,0 +1,11 @@
+const debugMode = false;
+const defaultBook = 'shijing';
+const defaultFamilyName = '康';
+const nameAmount = 50;
+
+export {
+  debugMode,
+  defaultBook,
+  defaultFamilyName,
+  nameAmount,
+};
---
+++ src/scripts/debugTools.js
@@ -0,0 +1,3 @@
+import { debugMode } from './config';
+
+export const log = console.log.bind(console);
